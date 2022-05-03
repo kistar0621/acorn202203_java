51efd3642b600f25953c823921fe9e57d95773db
leekisaung <kistar0621@naver.com>
2022-05-03 15:17:08 +0900

    Step13~14 commit

---
+++ Step13_Exception/src/test/main/MainClass06.java
@@ -0,0 +1,18 @@
+package test.main;
+
+import test.mypac.MyUtil;
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		//Exception 을 처리할 필요가 없음
+		MyUtil.draw();
+		
+		//send() 메소드에서 발생하는 Exception 을 처리해야한다.
+		try {
+			MyUtil.send();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Step14_Swing/src/test/main/MainClass02.java
@@ -0,0 +1,11 @@
+package test.main;
+
+import javax.swing.JFrame;
+
+import test.mypac.TestFrame;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		JFrame f=new TestFrame("Hello Frame");
+	}
+}
